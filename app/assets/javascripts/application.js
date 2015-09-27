4b4d49992f8eb0d1a60d234b51ab5cc0c3cfdf05
nonphotoblue22 <seville.demo@gmail.com>
2015-09-27 12:16:28 -0700

    vendor files for offcanvas nav when there's more time + nav styling +768+ breakpoint

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require jasny-bootstrap
 //= require_tree .
---
+++ app/assets/javascripts/offcanvas.js
@@ -0,0 +1,16 @@
+// 
+// $(document).ready(function () {
+//
+//   // Enter your ids or classes
+//   var toggler = '.navbar-toggle';
+//   var pagewrapper = '#content-wrap';
+//   var navigationwrapper = '.navbar-header';
+//
+//   $("#slide-nav").on("click", toggler, function (e) {
+//     var selected = $(this).hasClass('slide-active');
+//     $('#content-wrap, .navbar, body, .navbar-header').toggleClass('slide-active');
+//   });
+//
+//   var selected = '#slidemenu, #page-content, body, .navbar, .navbar-header';
+//
+// });
