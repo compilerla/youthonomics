b4bc1cc534928ec9bfdd1e1df5e983e47aebb109
Stephen Corwin <corwinstephen@gmail.com>
2015-09-29 13:28:35 -0700

    Added map

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,5 +4,8 @@ class CountriesController < ApplicationController
   end
 
   def show
+    @country = Country.find(params[:id])
+
+    render :cms_layout => 'country', :cms_blocks => @country.as_json
   end
 end
---
+++ db/cms_fixtures/youthonomics/layouts/default/country/javascript.js
@@ -0,0 +1,5 @@
+// country page js
+
+$(document).ready(function(){
+  console.log('this is running from the country page js');
+});
