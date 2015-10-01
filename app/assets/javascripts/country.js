e1fb3eac4bbf72b61eb6d875471f06bb66c9d677
nonphotoblue22 <seville.demo@gmail.com>
2015-09-30 23:49:37 -0700

    added simple fade in out animations on js class addition

--- app/assets/javascripts/country.js
+++ app/assets/javascripts/country.js
@@ -10,4 +10,17 @@ $(document).ready(function(){
   //       console.log(data) // log it
   //   }
   // });
+  $('body').addClass('js');
+  $('.blog-panel').mouseover(function(){
+    var cta = $(this).find('.check-it-cta');
+    console.log(cta);
+    cta.removeClass('fadeOutLeft');
+    cta.addClass('fadeInLeft');
+  });
+  $('.blog-panel').mouseleave(function(){
+    var cta = $(this).find('.check-it-cta');
+    console.log(cta);
+    cta.removeClass('fadeInLeft');
+    cta.addClass('fadeOutLeft');
+  });
 });
