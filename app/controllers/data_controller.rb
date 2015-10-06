7ed50b76e93eae9c0624dab3fc0f3c50a7517e87
Stephen Corwin <corwinstephen@gmail.com>
2015-10-06 14:13:01 -0400

    Remove data controller

--- app/controllers/data_controller.rb
+++
@@ -1,9 +0,0 @@
-class DataController < ApplicationController
-@data = File.read("/app/assets/javascript/youtho_index_main_metrics.json")
-
-def index
-  render :json => @data
- end
-end
-
-#= http://stackoverflow.com/questions/11149430/how-to-serve-a-static-json-file-in-rails
