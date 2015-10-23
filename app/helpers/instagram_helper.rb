f8be0007b958495b89df0e26e4cb2828fa3f16b7
Stephen Corwin <corwinstephen@gmail.com>
2015-10-23 17:38:34 -0400

    Restore key

--- app/helpers/instagram_helper.rb
+++ app/helpers/instagram_helper.rb
@@ -1,7 +1,7 @@
 module InstagramHelper
   def instagram_photos
     api_key = ENV['INSTAGRAM_API_TOKEN']
-    feed = JSON.parse(RestClient.get("https://api.instagram.com/v1/users/1921107209/media/recent/?client_id=39100473c9834b84b5c1c3a560e60945", { accept: :json }))
+    feed = JSON.parse(RestClient.get("https://api.instagram.com/v1/users/self/media/recent/?access_token=#{api_key}", { accept: :json }))
     return feed.to_json.html_safe
   end
 end
