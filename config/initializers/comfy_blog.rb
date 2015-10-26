577a0a7822338953e63fbb614e2b1fce2785041a
Stephen Corwin <corwinstephen@gmail.com>
2015-10-26 18:00:34 -0400

    Pagination

--- config/initializers/comfy_blog.rb
+++ config/initializers/comfy_blog.rb
@@ -1,7 +1,7 @@
 ComfyBlog.configure do |config|
 
   # Number of posts per page. Default is 10
-  #   config.posts_per_page = 10
+  config.posts_per_page = 3
 
   # Comments can be automatically approved/published by changing this setting
   # Default is false.
