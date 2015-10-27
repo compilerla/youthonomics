9412883e548c680e1eb4ce09fb2fe536f6b2471b
nonphotoblue22 <seville.demo@gmail.com>
2015-10-27 13:07:11 -0700

    added ig feed back in

--- app/helpers/instagram_helper.rb
+++ app/helpers/instagram_helper.rb
@@ -1,7 +1,7 @@
 module InstagramHelper
   def instagram_photos
     api_key = ENV['INSTAGRAM_API_TOKEN']
-    feed = []
+    feed = JSON.parse(RestClient.get("https://api.instagram.com/v1/users/self/media/recent/?access_token=#{api_key}", { accept: :json }))
     return feed.to_json.html_safe
   end
 end
